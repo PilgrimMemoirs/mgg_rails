ebbe0699e239ab16d948e3b9190c537518bfe22d
Jamie P <jamieleepilgrim@gmail.com>
2018-10-06 15:39:04 -0700

    setup hikes index and show wip

--- test/controllers/hikes_controller_test.rb
+++ test/controllers/hikes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HikeControllerTest < ActionDispatch::IntegrationTest
+class HikesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
